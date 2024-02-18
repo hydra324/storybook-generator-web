60f6b8042657fae23efdc0cbd7360b41e0db8478
hydra324 <akhilchilumuru@gmail.com>
2024-02-18 02:01:23 -0600

    started refactoring book to add flip animations

--- src/Book.js
+++ src/Book.js
@@ -1,29 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Page from './Page';
 import './Book.css';
+import emptyCanvasIllustration from './assets/undraw_mic_drop_uuyg.svg';
+
+const endingPage = 
+  <div className="page">
+    <img src={emptyCanvasIllustration} />
+  </div>
 
 const Book = () => {
-  const [leftPageIndex, setleftPageIndex] = useState(0);
+  const [allPages, setAllPages] = useState(() => []);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  // Function to handle page turn
-  const handleNextPageTurn = () => {
-    setleftPageIndex((prevIndex) => prevIndex + 2);
-  };
+  const setPage = (index) => {
+    // code to animate the page flip
+    // check whether index is within bounds
+    if (index < 0 || index >= allPages.length) return;
+    setCurrentPage(index);
+  }
 
-  const handlePreviousPageTurn = () => {
-    setleftPageIndex((prevIndex) => prevIndex - 2);
+  const createNewPage = () => {
+    setPage(allPages.length+1);
+    setAllPages(prevPages => [...prevPages, <Page pageIndex={prevPages.length} />]);
   };
 
   return (
     <div className="book">
       <div className="page-container">
-        <Page pageIndex={leftPageIndex} />
-        <Page pageIndex={leftPageIndex + 1} />
+        {allPages.map((page, index) => (
+            <div key={index} className="page-container-mid" style={{ zIndex: currentPage === index ? 1 : 0 }}>
+            {page}
+          </div>
+        ))}
       </div>
       <span className='page-buttons'>
-        <button onClick={handlePreviousPageTurn}>Previous Page</button>
-        <button onClick={handleNextPageTurn}>Next Page</button>
+        <button onClick={() => setPage(currentPage+1)}>Previous Page</button>
+        <button onClick={() => setPage(currentPage-1)}>Next Page</button>
       </span>
+      <button onClick={createNewPage}>Insert Page at the end</button>
     </div>
   );
 };
