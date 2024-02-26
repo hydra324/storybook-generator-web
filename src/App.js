1cc3348e7bd7e0626b735cf20154d78ece4eb278
hydra324 <akhilchilumuru@gmail.com>
2024-02-26 00:14:24 -0600

    added storybook, added chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Book from './Book';
+import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
   return (
-    <div className="App">
+    <ChakraProvider>
       <Book />
-    </div>
+    </ChakraProvider>
   );
 }
 
