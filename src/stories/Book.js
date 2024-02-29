1698bdf09189e45e9bdb2b69fed744fa6fb8ad6a
hydra324 <akhilchilumuru@gmail.com>
2024-02-29 01:51:03 -0600

    moved ai button to BookPage component

--- src/stories/Book.js
+++ src/stories/Book.js
@@ -9,29 +9,10 @@ const Book = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
 
-  const getImages = () => {
-    // get current focused page
-    // get images for that page
-    // set images for that page
-  }
+  
 
   return (
-    
       <VStack>
-          {totalPages > 0 &&
-          <IconButton 
-              variant='solid'
-              size='lg'
-              fontSize='35px'
-              color={theme.colors.white}
-              aria-label="AI Image"
-              pos="fixed"
-              bottom="10px"
-              right="10%"
-              icon={<Icon as={AIImageIcon} />}
-              onClick={getImages}
-            />
-          }
           {totalPages > 0 && Array.from({ length: totalPages }).map((_, index) => (
               <BookPage key={index} pageNumber={index + 1} />
           ))}
--- src/stories/BookPage.js
+++ src/stories/BookPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { HStack, IconButton, VStack } from '@chakra-ui/react';
+import { HStack, IconButton, VStack, Icon } from '@chakra-ui/react';
 import EmblaCarousel from './EmblaCarousel';
 import Page from './Page';
+import {AIImageIcon} from './AIImageIcon';
+import theme from '../theme';
 
 
-const BookPage = ({pageNumber, generateImages}) => {
+const BookPage = ({pageNumber}) => {
   const OPTIONS = {}
   // const SLIDES = [
   //   'https://img.freepik.com/free-vector/kids-discussing-with-book-forest_1308-30690.jpg?size=626&ext=jpg',
@@ -12,18 +14,38 @@ const BookPage = ({pageNumber, generateImages}) => {
   //   'https://img.freepik.com/free-vector/children-wild-animals-field_1308-31671.jpg?size=626&ext=jpg&ga=GA1.1.1039114739.1705691503&semt=ais',
     
   // ]
+
+  
   const [slides, setSlides] = React.useState([]);
+
+  const getImages = () => {
+    // get images from api
+    // set images from api
+  }
   return (
-    <HStack pos="relative" w="50%" h="100vh" m="auto">
+    <VStack pos="relative" w="50%" h="100vh" m="auto">
+        {
+            <IconButton 
+            variant='solid'
+            size='lg'
+            fontSize='35px'
+            color={theme.colors.white}
+            aria-label="AI Image"
+            pos="absolute"
+            bottom="0.3rem"
+            right="-4rem"
+            icon={<Icon as={AIImageIcon} />}
+            onClick={() => {}}
+          />
+        }
         {slides.length>0 && <EmblaCarousel slides={slides} options={OPTIONS} />}
         <Page pageNumber={pageNumber} />
-    </HStack>
+    </VStack>
   );
 };
 
 BookPage.defaultProps = {
-  pageNumber: 1,
-  generateImages: () => {}
+  pageNumber: 1
 };
 
 export default BookPage;
