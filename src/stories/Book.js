32b7535aca3ff1f57981aaa3745b52238b2d363e
hydra324 <akhilchilumuru@gmail.com>
2024-03-01 16:16:47 -0600

    added new page, auto scroll to bottom

--- src/stories/Book.js
+++ src/stories/Book.js
@@ -1,14 +1,50 @@
 import { VStack} from '@chakra-ui/react';
-import {React} from 'react';
+import {React, useState, useRef, useEffect} from 'react';
 import BookPage from './BookPage';
+import {IconButton, Text } from '@chakra-ui/react';
+import {AddIcon} from '@chakra-ui/icons';
 
 const Book = () => {
-  const totalPages = 3;
+    
+const pageRef = useRef(null);
+  const [totalPages, setTotalPages] = useState(0);
+  useEffect(() => {
+    if (pageRef.current) {
+      pageRef.current.scrollIntoView(
+        {
+          behavior: 'smooth',
+          block: 'end',
+          inline: 'nearest'
+        })
+    }
+  },
+  [totalPages]);
+  const addPage = () => {
+    setTotalPages(totalPages + 1);
+    // scroll to bottom
+  }
   return (
-      <VStack className='book-stack'>
+      <VStack className='book-stack' ref={pageRef}>
           {totalPages > 0 && Array.from({ length: totalPages }).map((_, index) => (
               <BookPage key={index} pageNumber={index + 1} />
           ))}
+          {totalPages === 0 && <Text fontSize='lg' marginTop='25%'>Add a page to start</Text>}
+          {
+            <IconButton 
+            variant='solid'
+            size='lg'
+            fontSize='25px'
+            // color='brand.200'
+            bg='brand.200'
+            _hover={{bg: 'brand.100'}}   
+            aria-label="Add New Page"
+            pos="fixed"
+            top="0.3rem"
+            right="1rem"
+            icon={<AddIcon />}
+            onClick={addPage}
+          />
+        }
       </VStack>
     
     
